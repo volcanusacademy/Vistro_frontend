305181edf1871079f5523a98743f3ee6d61f27cf
unknown <rajul.soni@volcanus.in>
2024-04-10 17:28:07 +0530

    modified code

--- dist/assets/Error-a29597bf.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-f31ace3e.js";const e=()=>r("div",{children:r("h1",{children:"Error"})});export{e as default};
--- dist/assets/config-cb20809b.js
+++
@@ -1,1 +0,0 @@
-const t="http://20.193.128.134";export{t as B};
